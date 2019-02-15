4d000510720a9fdfd5b255ccebdffcbdb60978fd
Johnathan Kupferer <jkupfere@redhat.com>
2019-02-15 10:05:27 -0600

    Represent changes with json blocks in change report

--- filter_plugins/change_record.py
+++ filter_plugins/change_record.py
@@ -1,5 +1,5 @@
+import json
 import re
-import yaml
 
 def is_connection_opt(s):
     m = re.match(r'--([a-z-]+)(=|$)', s)
@@ -36,12 +36,12 @@ def format_change_command(value):
 def format_change_provision(value):
     kind = value['resource']['kind']
     change = {
-        'action': value['action'],
-        'kind': kind,
-        'name': value['resource']['metadata']['name']
+        'action': str(value['action']),
+        'kind': str(kind),
+        'name': str(value['resource']['metadata']['name'])
     }
     if 'namespace' in value['resource']['metadata']:
-        change['namespace'] = value['resource']['metadata']['namespace']
+        change['namespace'] = str(value['resource']['metadata']['namespace'])
     if kind != 'Secret':
         if value.get('patch', None):
             change['patch'] = value['patch']
@@ -51,12 +51,24 @@ def format_change_provision(value):
 
 def record_change(change, change_record):
     fh = open(change_record, 'a')
-    yaml.safe_dump(
-        change,
-        fh,
-        default_flow_style=False,
-        explicit_start=True
-    )
+    fh.write("---\n")
+    for k in sorted(change):
+        v = change[k]
+        if isinstance(v, str):
+            fh.write("{}: {}\n".format(k, v))
+        elif k == 'command':
+            fh.write("command: {}\n".format(
+                 ' '.join(v)
+            ))
+        else:
+            fh.write("{}: |\n  {}\n".format(
+                k,
+                json.dumps(
+                    v,
+                    indent=2,
+                    separators=(',',': ')
+                ).replace("\n", "\n  ")
+            ))
 
 def record_change_command(value, change_record=''):
     if change_record:
