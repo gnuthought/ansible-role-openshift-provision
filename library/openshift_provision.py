gnuthought/ansible-role-openshift-provision

commit 216e7dd8bfc0b3f4459c7f178e9136ad97484f32

use resource fqdn when running patch and delete
If there are multiple resources with the same `Kind` but are part of
different API groups, you have to provide the API group and the Kind
in the format `kind.apigroup` to ensure the correct resource is
modified.

For example, in OpenShift 4.x, there are `configs.operator.openshift.io`
and `configs.imageregistry.operator.openshift.io` resources. If you
try to run `oc patch configs cluster...`, the API call would not know for
sure if you were trying to patch the image registry config, or the
operator config and can fail.

To prevent issues, you can provide the full resource kind in your
command, for example to patch the image registry, you should use
`oc patch configs.imageregistry.operator.openshift.io cluster...`

Fixes #19

library/openshift_provision.py
@@ -1465,13 +1465,14 @@ class OpenShiftProvision:
             return
 
         # Perform action on resource
+        resource_fqdn = self.resource['kind'] + "." + self.resource['apiVersion'].split("/")[0]
         if self.action == 'delete':
-            command = ['delete', self.resource['kind'], self.resource['metadata']['name']]
+            command = ['delete', resource_fqdn, self.resource['metadata']['name']]
             if self.namespace:
                 command += ['-n', self.namespace]
             (rc, stdout, stderr) = self.run_oc(command, check_rc=True)
         elif self.action == 'patch':
-            command = ['patch', self.resource['kind'], self.resource['metadata']['name'],
+            command = ['patch', resource_fqdn, self.resource['metadata']['name'],
                 '--patch=' + json.dumps(self.resource),
                 '--type=' + self.patch_type
             ]

commit 14c929ab16547f87c25822397ff138d5f61030f1

Fixing Patch and Delete For v1 API Group
Commit 216e7dd8bfc0b3f4459c7f178e9136ad97484f32 broke patching and
deleting resouces in the v1 API group(i.e. Service Pod, Namespace).

library/openshift_provision.py
@@ -1467,15 +1467,25 @@ class OpenShiftProvision:
         # Perform action on resource
         resource_fqdn = self.resource['kind'] + "." + self.resource['apiVersion'].split("/")[0]
         if self.action == 'delete':
-            command = ['delete', resource_fqdn, self.resource['metadata']['name']]
+            if self.resource['apiVersion'].split("/")[0] == "v1":
+                command = ['delete', self.resource['kind'], self.resource['metadata']['name']]
+            else:
+                command = ['delete', resource_fqdn, self.resource['metadata']['name']]
+
             if self.namespace:
                 command += ['-n', self.namespace]
             (rc, stdout, stderr) = self.run_oc(command, check_rc=True)
         elif self.action == 'patch':
-            command = ['patch', resource_fqdn, self.resource['metadata']['name'],
-                '--patch=' + json.dumps(self.resource),
-                '--type=' + self.patch_type
-            ]
+            if self.resource['apiVersion'].split("/")[0] == "v1":
+                command = ['patch', self.resource['kind'], self.resource['metadata']['name'],
+                    '--patch=' + json.dumps(self.resource),
+                    '--type=' + self.patch_type
+                ]
+            else:
+                command = ['patch', resource_fqdn, self.resource['metadata']['name'],
+                    '--patch=' + json.dumps(self.resource),
+                    '--type=' + self.patch_type
+                ]
             if self.namespace:
                 command += ['-n', self.namespace]
             self.run_oc(command, check_rc=True)